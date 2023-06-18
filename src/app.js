fab1000aaba1ed4bbe6aa2bfe4b2d3ed00a01d76
thiagocalvi <thiagohcalvi@gmail.com>
2023-06-18 14:17:56 -0300

    ordenando os impots de app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,9 @@ import routes from './routes.js';
 import express from 'express';
 const app = express();
 
-//ROTAS
-app.use(routes)
-
 //indica para o express ler body com json
 app.use(express.json());
+//ROTAS
+app.use(routes)
 
 export default app;
